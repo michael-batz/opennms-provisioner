b2c4e7607acde0e2db843cc5dcb62b674bcc30de
Michael Batz <mail@michael-batz.de>
2018-01-08 14:18:57 +0100

    main.py: argument parser, read config from correct path

--- provisioner/main.py
+++ provisioner/main.py
@@ -7,8 +7,8 @@ This is the main module of opennms-provisioner
 :license: MIT, see LICENSE for more details
 :copyright: (c) 2018 by Michael Batz, see AUTHORS for more details
 """
-
-import sys
+import argparse
+import os
 import config
 import executor
 
@@ -16,11 +16,17 @@ def main():
     """main function"""
 
     # get config and JobUtilty
-    appconfig = config.AppConfig("../etc/appconfig.conf")
+    basedir = os.path.dirname(__file__)
+    appconfig = config.AppConfig(basedir + "/../etc/appconfig.conf")
     jobutil = executor.JobUtility(appconfig)
 
+    # parse arguments
+    parser =  argparse.ArgumentParser(description="Helper for OpenNMS Provisioning")
+    parser.add_argument("jobname", help="name of the provisioning job")
+    args = parser.parse_args()
+
     # get job
-    job = jobutil.create_job("default")
+    job = jobutil.create_job(args.jobname)
     job.execute()
 
 
